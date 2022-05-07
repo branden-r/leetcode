f25b089a3e10f1f520549e0082d1bea0274adddf
bird.ron <bird.ron@pm.me>
2022-05-07 08:54:04 -0700

    Completed Isomorphic Strings.

---
+++ isomorphic_strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        result: bool = True
+        mapping: dict[str, str] = {}
+        s_char: str
+        t_char: str
+        for s_char, t_char in zip(s, t):
+            if s_char not in mapping and t_char not in mapping.values():  # if both s and t are free, map them
+                mapping[s_char] = t_char
+            elif s_char not in mapping or mapping[s_char] != t_char:
+                result = False
+                break
+        return result
